947715c83e7bcc9540d77d63bf3c754e55684874
mmaghakian <matthewmaghakian@gmail.com>
2023-03-24 19:52:13 -0400

    new build

--- portfolio/src/components/Labs.jsx
+++ portfolio/src/components/Labs.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-
 import styled from 'styled-components'
+
 function Button({ labNumber }) {
   return (
     <Link to={`/labs/${labNumber}`}>
