f4992a00508bc732ac9fde801097ed31469c1cd7
mmaghakian <matthewmaghakian@gmail.com>
2023-03-25 01:01:10 -0400

    update

--- portfolio/src/components/LabData/Lab.data.js
+++ portfolio/src/components/LabData/Lab.data.js
@@ -1,13 +1,15 @@
 export const Lab1 = {
     name: "lab 1",
     num: 1,
-    description: "The quick brown fox jumped over the lazy dog",
+    title: "Discussion Post - Web Usability.",
+    description: "In making this website, the core concepts of web usability were followed. It was designed mobile-first, making every part of the experience responsive. Parts of the site that do not fit on mobile layouts are removed at a certain screen width. The site makes sure to use minimal fonts, colours, and images to stay minimal and run smoothly on all platforms. This is important when considering the variety of mobile devices that may access the site.",
     date: "2023-03-24"
 }
 
 export const Lab2 = {
     name: "lab 2",
     num: 2,
+    title: "",
     description: "The quick brown fox jumped over the lazy dog",
     date: "2023-03-24"
 }
@@ -15,6 +17,7 @@ export const Lab2 = {
 export const Lab3 = {
     name: "lab 3",
     num: 3,
+    title: "",
     description: "The quick brown fox jumped over the lazy dog",
     date: "2023-03-24"
 }
@@ -22,6 +25,8 @@ export const Lab3 = {
 export const Lab4 = {
     name: "lab 4",
     num: 4,
+    title: "",
     description: "The quick brown fox jumped over the lazy dog",
     date: "2023-03-24"
 }
+
